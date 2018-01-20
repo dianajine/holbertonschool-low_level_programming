09462091f6c5a106b70c67fdbd5e2988639add49
Diana Jine <diana.jine@gmail.com>
2018-01-20 18:56:37 +0000

    modified file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int putchar)
 {
-printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
