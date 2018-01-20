ee7f72fdd88085ad2b84dd703a4cc5b4830adeb7
Diana Jine <diana.jine@gmail.com>
2018-01-20 20:47:42 +0000

    modified file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/*Simple program to show size of data types.*/
 int main(void)
 {
 printf("Size of a char: %d bytes\n", sizeof(char));
@@ -6,5 +7,5 @@ printf("Size of an int: %d bytes\n", sizeof(int));
 printf("Size of a long int: %d bytes\n", sizeof(long int));
 printf("Size of a long long int: %d bytes\n", sizeof(long long int));
 printf("Size of a float: %d bytes\n", sizeof(float));
- return (0)
+ return (0);
 }
