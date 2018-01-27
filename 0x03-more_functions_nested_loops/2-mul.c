097ca974a45f174574c9f6d162274c13964720f1
Diana Jine <diana.jine@gmail.com>
2018-01-27 23:58:59 +0000

    initial commit

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+(
+result = (a * b);
+return (result);
+}
